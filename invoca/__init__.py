11d0c11370547d9080e7b9efdfbd34761ee534a1
David Jenkins <david.nicholas.jenkins@gmail.com>
2018-03-09 17:14:08 -0500

    Updated version. Bumping to full version since this is successfully being used in production via the transactions api

--- invoca/__init__.py
+++ invoca/__init__.py
@@ -3,7 +3,7 @@ import logging
 from .api import Invoca
 from .exceptions import InvocaException, UnsupportedApiVersionError
 
-__version__ = '1.0.0b2'
+__version__ = '1.0.1'
 
 # Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
